60763b6fdbd6718f78a371e8c9d070ee8fc4c12a
AnggaCristanto <210411100039@trunojoyo.ac.id>
2022-08-29 10:48:22 +0700

    first commit

---
+++ baru.php
@@ -0,0 +1,7 @@
+<?php
+    for ($i=0: $i<1000 ; $i++) {
+
+        echo ("<br> selamat anda berhasil",$i);
+        
+    }
+?>
---
+++ belajar.php
@@ -0,0 +1,9 @@
+<?php
+    echo "selamat anda berhasil";
+?>
+<br>
+<p>
+    <h2>ni paragraf</h2>
+</p>
+ini adalah text biasa
+background-color : blue;
